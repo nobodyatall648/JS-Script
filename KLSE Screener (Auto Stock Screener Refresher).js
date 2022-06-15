b48afde15f16c332e88a9fa67f68c311cbe2ee1e
nobodyatall648 <35725871+nobodyatall648@users.noreply.github.com>
2022-06-15 23:47:55 +0800

    Update KLSE Screener (Auto Stock Screener Refresher).js

--- KLSE Screener (Auto Stock Screener Refresher).js
+++ KLSE Screener (Auto Stock Screener Refresher).js
@@ -1,4 +1,5 @@
 //auto click the screen button in KLSE Screener (Stock Screener) within the time interval
+//https://www.klsescreener.com/v2/
 javascript: setInterval(function auto() {
     console.log('[*] Auto Screener Start.');
     $('#submit').click();
